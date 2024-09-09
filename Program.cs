614dbb66a253d8723a2642c5d8517becccc7818f
Stephano Chang <stephanochang21@gmail.com>
2024-09-08 22:02:18 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,26 @@
 using Hangfire;
 using JazzApi;
 using JazzApi.Hangfire;
-Console.WriteLine("Inicio Program");
-var builder = WebApplication.CreateBuilder(args);
-Console.WriteLine("Inicio llamado al star up");
-var startup = new Startup(builder.Configuration);
-Console.WriteLine("fin llamado al star up");
-startup.ConfigureServices(builder.Services);
-Console.WriteLine("Configurando servicios");
-var app = builder.Build();
-Console.WriteLine("Construyendo servicios");
-var backgroundJobClient = app.Services.GetRequiredService<IBackgroundJobClient>();
-var hangfireService = (HangfireService)app.Services.GetService(typeof(HangfireService));
-startup.Configure(app, app.Environment, backgroundJobClient, hangfireService);
-Console.WriteLine("Fin Program");
+try
+{
+    Console.WriteLine("Inicio Program");
+    var builder = WebApplication.CreateBuilder(args);
+    Console.WriteLine("Inicio llamado al star up");
+    var startup = new Startup(builder.Configuration);
+    Console.WriteLine("fin llamado al star up");
+    startup.ConfigureServices(builder.Services);
+    Console.WriteLine("Configurando servicios");
+    var app = builder.Build();
+    Console.WriteLine("Construyendo servicios");
+    var backgroundJobClient = app.Services.GetRequiredService<IBackgroundJobClient>();
+    var hangfireService = (HangfireService)app.Services.GetService(typeof(HangfireService));
+    startup.Configure(app, app.Environment, backgroundJobClient, hangfireService);
+    Console.WriteLine("Fin Program");
 
-app.Run();
+    app.Run();
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine("Error " + ex.Message);
+}
