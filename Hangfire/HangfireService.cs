9e82fbceb787be4f5abf817c3e6e91780aa1ba10
Stephano Chang <stephanochang21@gmail.com>
2024-09-29 22:25:56 -0500

    Down Grade To SDK 7.0

--- Hangfire/HangfireService.cs
+++ Hangfire/HangfireService.cs
@@ -27,19 +27,7 @@ namespace JazzApi.Hangfire
 
                 _started = true;
 
-                RecurringJob.RemoveIfExists("Recargar Página");
                 RecurringJob.RemoveIfExists("Recargar Factura");
-                RecurringJob.RemoveIfExists("Actualizar Producto Swiss");
-                RecurringJob.RemoveIfExists("Actualizar Producto Swiss Inactivos");
-                RecurringJob.RemoveIfExists("Actualizar Catalogo Swiss");
-                RecurringJob.RemoveIfExists("Eliminar logs");
-                RecurringJob.RemoveIfExists("Sincronizar Solicitudes TMS");
-                RecurringJob.RemoveIfExists("Sincronizar Solicitudes Bloqueadas TMS");
-                RecurringJob.RemoveIfExists("Verificar Vencimiento Remuneración Variable");
-                RecurringJob.RemoveIfExists("Recargar Factura");
-
-               
-
                 if (Debugger.IsAttached)
                 {
                     //RecurringJob.AddOrUpdate("Recargar Página", () => this.CargarSitio(), "*/4 * * * *");
@@ -49,33 +37,6 @@ namespace JazzApi.Hangfire
             }
         }
 
-        public long CargarSitio()
-        {
-            var MyConfig = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json").Build();
-
-            //var AppName = MyConfig.GetValue<string>("urldomain");
-
-            var systemDomain = MyConfig.GetValue<string>("urldomain") ?? "https://conautecqa.apptelink.com/";
-
-            var wr = WebRequest.Create(systemDomain);
-            var sw = new Stopwatch();
-
-            sw.Start();
-            wr.Timeout = 3500;
-
-            try
-            {
-                HttpWebResponse response = (HttpWebResponse)wr.GetResponse();
-                sw.Stop();
-            }
-            catch (Exception ex)
-            {
-                sw.Stop();
-                //We know its going to fail but that dosent matter!!
-            }
-            return sw.ElapsedMilliseconds;
-        }
-        [AutomaticRetry(Attempts = 0)]
         //[DisableMultipleQueuedItemsFilter]
         public async Task CargarFacturas()
         {
